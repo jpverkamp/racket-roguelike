4d0df23cc5c6eb5a332f4bb7c0ba2db75a1c3117
JP Verkamp <verkampj@indiana.edu>
2013-04-11 05:49:19 -0400

    Added basic scrolling map generation.

--- src/game-screen.rkt
+++ src/game-screen.rkt
@@ -4,7 +4,8 @@
  game-screen%)
 
 (require 
- "screen.rkt")
+ "screen.rkt"
+ "noise/noise.rkt")
 
 ; API to use imaginary numbers as points
 (define pt make-rectangular)
@@ -21,6 +22,19 @@
     ; Store the player's state
     ; Use an imaginary number for a point
     (define player (pt 0 0))
+    
+    ; Get the contents of a given point, caching for future use
+    ; Hash on (x y) => char
+    (define caves (make-hash))
+    (define (get-tile x y)
+      (unless (hash-has-key? caves (list x y))
+        (hash-set! caves (list x y)
+          (let ()
+            (define wall? (> (simplex (* 0.1 x) (* 0.1 y)) 0))
+            (cond
+              [wall? 'wall]
+              [else  'empty]))))
+      (hash-ref caves (list x y)))
     
     ; Process keyboard events
     (define/override (update key-event)
@@ -39,9 +53,21 @@
     (define/override (draw canvas)
       (send canvas clear)
       
-      ; Draw the player
-      ; 0x0 is the center point of the canvas
-      (let ([player (recenter canvas player)])
-        (send canvas write #\@ (pt-x player) (pt-y player))))
+      ; Draw some caverns around the player
+      (for* ([xi (in-range (send canvas get-width-in-characters))]
+             [yi (in-range (send canvas get-height-in-characters))])
+        (define x/y (recenter canvas (+ (pt xi yi) player)))
+        (case (get-tile (pt-x x/y) (pt-y x/y))
+          [(wall) (send canvas write #\# xi yi)]))
+      
+      ; Draw the player centered on the screen
+      (let ([player (recenter canvas (pt 0 0))])
+        (send canvas write #\@ (pt-x player) (pt-y player)))
+      
+      ; Debug: Show the player location
+      (send canvas write-string
+            (format "~a, ~a" (pt-x player) (pt-y player))
+            1 1
+            "green"))
     
     (super-new)))
