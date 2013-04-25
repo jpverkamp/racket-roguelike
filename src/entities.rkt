4b8645c73688405a25a5ed4c97112260927154d1
JP Verkamp <verkampj@indiana.edu>
2013-04-25 02:35:20 -0400

    Added seeking and fleeing enemies.

--- src/entities.rkt
+++ src/entities.rkt
@@ -36,10 +36,54 @@
             (pt (- (random 3) 1)
                 (- (random 3) 1))))])
 
+; A seeking enemy runs towards the player (heedless of walls) 50% of the time
+; The other 50% of the time they are identical to a wandering enemy
+(define-thing seeking-enemy wandering-enemy
+  [(act me world)
+   (cond
+     ; 50/50 of a seeking move
+     [(= 0 (random 2))
+      (define player-pt (thing-get (send world get-player) 'location))
+      (define me-pt (thing-get me 'location))
+      (define dir (unit (- player-pt me-pt)))
+      (send world try-move
+            me
+            (+ me-pt
+               (inexact->exact (round (pt-x dir)))
+               (inexact->exact (round (pt-y dir)))))]
+     ; Otherwise, wander
+     [else
+      (thing-call wandering-enemy 'act me world)])])
+
+; A fleeing enemy moves the exact opposite of a seeking enemy
+(define-thing fleeing-enemy wandering-enemy
+  [(act me world)
+   (cond
+     ; 50/50 of a fleeing move
+     [(= 0 (random 2))
+      (define player-pt (thing-get (send world get-player) 'location))
+      (define me-pt (thing-get me 'location))
+      (define dir (unit (- player-pt me-pt)))
+      (send world try-move
+            me
+            (- me-pt
+               (inexact->exact (round (pt-x dir)))
+               (inexact->exact (round (pt-y dir)))))]
+     ; Otherwise, wander
+     [else
+      (thing-call wandering-enemy 'act me world)])])
+      
+
 ; A list of random enemies that we can generate
 (define random-enemies
   (vector
-   (make-thing wandering-enemy
+   (make-thing fleeing-enemy
      [name "rat"]
-     [character #\r])))
+     [character #\r])
+   (make-thing seeking-enemy
+     [name "goblin"]
+     [character #\g]
+     [color "orange"]
+     [attack 15]
+     [defense 5])))
     
--- src/point.rkt
+++ src/point.rkt
@@ -11,3 +11,16 @@
 (define (recenter canvas orig)
   (+ orig (pt (quotient (send canvas get-width-in-characters) 2)
               (quotient (send canvas get-height-in-characters) 2))))
+
+; Calculate the distance between two points
+(define (distance p1 p2)
+  (define (sqr n) (* n n))
+  (sqrt (+ (sqr (- (pt-x p2) (pt-x p1)))
+           (sqr (- (pt-y p2) (pt-y p1))))))
+  
+
+; Convert a point to a unit point (distance is one for origin)
+(define (unit p)
+  (define d (distance 0 p))
+  (pt (/ (pt-x p) d)
+      (/ (pt-y p) d)))
